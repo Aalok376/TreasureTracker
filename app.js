4e5174d9a9c935158ebbd345bceab026914b925c
Aalok376 <yadavaalok453@gmail.com>
2025-01-20 21:02:40 +0545

    mongo issue resolved

--- app.js
+++ app.js
@@ -19,29 +19,6 @@ mongoose.connect(process.env.MONGO_URI1)
     .then(() => console.log("Connected to DB1"))
     .catch(err => console.log("Error connecting to DB1", err));
 
-const db2=mongoose.createConnection(process.env.MONGO_URI2)
-db2.on('connected',()=>{
-    console.log('Connected to db2')
-})
-db2.on('error',(error)=>{
-    console.log('Error connecting to db2',error)
-})
-
-const db3=mongoose.createConnection(process.env.MONGO_URI3)
-db3.on('connected',()=>{
-    console.log('Connected to db3')
-})
-db3.on('error',(error)=>{
-    console.log('Error connecting to db3',error)
-})
-
-const db4=mongoose.createConnection(process.env.MONGO_URI4)
-db4.on('connected',()=>{
-    console.log('Connected to db4')
-})
-db4.on('error',(error)=>{
-    console.log('Error connecting to db4',error)
-})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening on port ${process.env.PORT}`)
