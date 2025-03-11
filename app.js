571a06ed4411de2e21a3b3648ecd0a7e856ab1e3
Aalok376 <yadavaalok453@gmail.com>
2025-03-11 18:10:06 +0545

    hello

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-// app.use(express.static(path.join(__dirname,'./docs')))
-// app.use('/uploads',express.static(path.join(__dirname,'./uploads')))
+app.use(express.static(path.join(__dirname,'./docs')))
+app.use('/uploads',express.static(path.join(__dirname,'./uploads')))
 
 app.use('/api/v1',router)
 
