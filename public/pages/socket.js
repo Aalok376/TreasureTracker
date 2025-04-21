62acfa488123f3e8debdf0020eafe5a6cbda908e
Aalok376 <yadavaalok453@gmail.com>
2025-04-21 12:46:48 +0545

    Update socket.js

--- public/pages/socket.js
+++ public/pages/socket.js
@@ -1,4 +1,4 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 
 const socket = io("https://treasure-tracker-pi.vercel.app", {
   reconnection: true,
@@ -6,14 +6,14 @@ const socket = io("https://treasure-tracker-pi.vercel.app", {
   reconnectionDelay: 500,
   reconnectionDelayMax: 1000,
   randomizationFactor: 0
-})
+});
 
 socket.on('connect', () => {
-  console.log('Connected to server with ID:', socket.id)
-})
+  console.log('Connected to server with ID:', socket.id);
+});
 
 socket.on('disconnect', () => {
-  console.log('Disconnected from server')
-})
+  console.log('Disconnected from server');
+});
 
-export default socket
+export default socket;
