3ef2ec2f1f2c1ee7064e7ee7ed8e5f44443ad34a
Aalok376 <yadavaalok453@gmail.com>
2025-04-21 11:30:29 +0545

    vercel

--- models/sockets.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const socketSchema = new mongoose.Schema({
-    userId: String,
-    socketId: String
-})
-
-const SocketModel = mongoose.model('Socket', socketSchema)
-module.exports=SocketModel
