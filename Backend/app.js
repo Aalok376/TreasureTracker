e6b3699703ea518e313387f82c0ed10a9bc30b76
Aalok376 <yadavaalok453@gmail.com>
2025-04-21 12:56:15 +0545

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,10 @@ const express=require('express')
 const path=require('path')
 const app= express()
 const {router}=require('./routes/route')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
